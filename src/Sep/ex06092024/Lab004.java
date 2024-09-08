ef1e4a60c21c32c56a96a25bb53b456c0f89d626
drjcu <cu.16bcs1582@gmail.com>
2024-09-08 23:19:33 +0530

    new commit

--- src/Sep/ex06092024/Lab004.java
+++ src/Sep/ex06092024/Lab004.java
@@ -3,6 +3,7 @@ package Sep.ex06092024;
 public class Lab004 {
     public static void main (String[] args){
         System.out.println("Hello world!");
+        System.out.println(2+2);
     }
 
 }
