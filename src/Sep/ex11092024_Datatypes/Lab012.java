5a0bc29394dc8e741d60c06c616498c3ab84c085
drjcu <cu.16bcs1582@gmail.com>
2024-09-12 17:37:08 +0530

    new commit

--- src/Sep/ex11092024_Datatypes/Lab012.java
+++ src/Sep/ex11092024_Datatypes/Lab012.java
@@ -1,4 +1,10 @@
 package Sep.ex11092024_Datatypes;
 
 public class Lab012 {
+    public static void main(String[] args) {
+        // Write Code.
+        // data type variable_name = variable_value
+//        boolean is_married = True; - Wrong
+        boolean is_married = true;
+    }
 }
--- src/Sep/ex11092024_Datatypes/Lab016.java
+++ src/Sep/ex11092024_Datatypes/Lab016.java
@@ -1,4 +1,15 @@
 package Sep.ex11092024_Datatypes;
 
 public class Lab016 {
-}
+    public static void main(String[] args) {
+        // Store the distance between earth and moon
+        long distance_between_earth_moon = 384400000l; //in M
+        double distance_between_earth_moon2 = 384400000.865; //in M
+
+        //max age
+        byte age = 122;
+        int age1 = 100; // wasting memory - 214768348488
+
+        // int, double - 99%
+    }
+    }
